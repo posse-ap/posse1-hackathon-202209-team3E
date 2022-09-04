3ddaeeca439e420e4e12c6fc97ab2114c4b62b92
ryuhashi <hashimotoryuichi11@gmial.com>
2022-09-04 19:37:04 +0900

    ほぼ変化なし

--- assets/scripts/speech.js
+++ assets/scripts/speech.js
@@ -1,9 +1,9 @@
 'use strict';
 {
-
-
-
-
-
-  
+  $(function(){
+    $(".more").on("click", function() {
+      $(this).toggleClass("on-click");
+      $(".speech-contents-hide").slideToggle(1000);
+    });
+  }); 
 }
