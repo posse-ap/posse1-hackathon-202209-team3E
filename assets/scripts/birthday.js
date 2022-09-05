77e63b3158a5e63c8b694bbb8067d593845bb11e
ryuhashi <hashimotoryuichi11@gmial.com>
2022-09-05 23:03:41 +0900

    ボタン中央よせflex

--- assets/scripts/birthday.js
+++ assets/scripts/birthday.js
@@ -1,9 +1,9 @@
 'use strict';
-{
-
-
-
-
-
+// {
+//   const circle = document.querySelector('.circle');
+//   const addClass = ()=>{
+//       circle.classList.toggle('is-animated');
+//   }
   
-}
+//   circle.addEventListener('click',addClass ,false);
+// }
